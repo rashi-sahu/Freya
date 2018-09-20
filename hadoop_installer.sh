d8533f8196a400f78727145108800805a2659e8d
Abhishek Sah <abhi.sah.97@gmail.com>
2018-09-20 17:06:30 +0530

    [Abhishek] Adds java installer

---
+++ hadoop_installer.sh
@@ -0,0 +1,6 @@
+echo "Installing Java 8"
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt update
+sudo apt install oracle-java8-installer
+sudo apt install oracle-java8-set-default
+echo "Successfully Installed Java 8"
