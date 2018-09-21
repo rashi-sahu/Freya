ff95e99dd8c9e2ecf41d09f3599b5acfce2666e9
rashi-sahu <rashi.shau13@gmail.com>
2018-09-21 18:25:40 +0530

    [Rashi] Installs Apache-Storm in a single node

--- storm_installer.sh
+++ storm_installer.sh
@@ -14,4 +14,22 @@ initLimit=5
 syncLimit=2" >> conf/zoo.cfg
 echo "Zookeeper installation complete......"
 
+cd ..
+echo "Apache-Storm-1.2.2 installation......"
+echo "download Apache-Storm-1.2.2......"
+wget http://mirrors.wuchna.com/apachemirror/storm/apache-storm-1.2.2/apache-storm-1.2.2.tar.gz
+tar -xvf apache-storm-1.2.2.tar.gz
+cd apache-storm-1.2.2
+mkdir data
+echo "storm.zookeeper.servers:
+ - "localhost"
+storm.local.dir: “/path/to/storm/data(any path)”
+nimbus.host: "localhost"
+supervisor.slots.ports:
+ - 6700
+ - 6701
+ - 6702
+ - 6703" >> conf/storm.yaml
+ echo "Apache-storm installation complete......"
 
+ 
