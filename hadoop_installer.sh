94f8f994d74e63701c15422554c0a143c719ea61
Abhishek Sah <abhi.sah.97@gmail.com>
2018-09-21 16:33:45 +0530

    [Abhishek] Checks whether public key exists or not

--- hadoop_installer.sh
+++ hadoop_installer.sh
@@ -4,3 +4,10 @@ sudo apt update
 sudo apt install oracle-java8-installer
 sudo apt install oracle-java8-set-default
 echo "Successfully Installed Java 8"
+if [ -r ~/.ssh/id_rsa.pub ]; then
+    content=$(cat ~/.ssh/id_rsa.pub)
+    echo "Public key exists...."
+    echo $content
+else
+    echo "The file '~/.ssh/id_rsa.pub' does not exist."
+fi
