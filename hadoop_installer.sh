569a5fa6d96654bf4fbd9225900a61061d4ede28
Abhishek Sah <abhi.sah.97@gmail.com>
2018-09-21 19:17:48 +0530

    [Abhishek] Adds hadoop to path

--- hadoop_installer.sh
+++ hadoop_installer.sh
@@ -12,10 +12,19 @@ else
     ssh-keygen -t rsa -P ""
 fi
 echo "Copying public key to authorized keys"
-cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys 
+cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
 cd /usr/local
 sudo wget http://mirrors.wuchna.com/apachemirror/hadoop/common/hadoop-2.9.1/hadoop-2.9.1.tar.gz
 sudo tar xzf hadoop-2.9.1.tar.gz
 sudo mv hadoop-2.9.1 hadoop
 sudo rm -rf hadoop-2.9.1.tar.gz
 echo "export HADOOP_HOME=/usr/local/hadoop" >> ~/.bashrc
+echo "export JAVA_HOME=/usr/lib/jvm/java-8-oracle" >> ~/.bashrc
+echo "unalias fs &> /dev/null" >> ~/.bashrc
+echo 'alias fs="hadoop fs"' >> ~/.bashrc
+echo 'unalias hls &> /dev/null' >> ~/.bashrc
+echo 'alias hls="fs -ls"' >> ~/.bashrc
+echo 'lzohead () {' >> ~/.bashrc
+echo '  hadoop fs -cat $1 | lzop -dc | head -1000 | less' >> ~/.bashrc
+echo '}' >> ~/.bashrc
+echo 'export PATH=$PATH:$HADOOP_HOME/bin' >> ~/.bashrc
