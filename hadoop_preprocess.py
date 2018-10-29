d54e25ded2d81dcfea911f6c6374cf6934012bd6
Abhishek Sah <abhi.sah.97@gmail.com>
2018-10-29 15:40:53 +0530

    [Abhishek] Automates public key generation for each node in hadoop preprocessing

--- hadoop_preprocess.py
+++ hadoop_preprocess.py
@@ -1,3 +1,5 @@
+import os
+import sys
 original_data = ""
 new_data = ""
 with file('/etc/hosts', 'r') as original:
@@ -6,3 +8,5 @@ with file('./nodes.txt', 'r') as nodes:
     new_data = nodes.read()
 with file('/etc/hosts', 'w') as modified:
     modified.write(new_data + "\n" + original_data)
+
+os.system("./public_key_generate.sh")
