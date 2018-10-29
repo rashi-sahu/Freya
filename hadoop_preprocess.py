590d8a7f4afae14293bb81c89063223de155f7cf
Abhishek Sah <abhi.sah.97@gmail.com>
2018-10-29 15:27:33 +0530

    [Abhishek] Adds all ip/hostnames to hosts file

---
+++ hadoop_preprocess.py
@@ -0,0 +1,8 @@
+original_data = ""
+new_data = ""
+with file('/etc/hosts', 'r') as original:
+    original_data = original.read()
+with file('./nodes.txt', 'r') as nodes:
+    new_data = nodes.read()
+with file('/etc/hosts', 'w') as modified:
+    modified.write(new_data + "\n" + original_data)
