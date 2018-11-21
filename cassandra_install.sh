ebe40703aec788cc58b74c6e1cb6bad249d6a0ee
rashi-sahu <rashi.sahu13@gmail.com>
2018-11-21 16:46:57 +0530

    [Rashi] Install Cassandra

--- cassandra_install.sh
+++ cassandra_install.sh
@@ -1,4 +1,6 @@
-
+cd /usr/local
 sudo wget http://mirrors.wuchna.com/apachemirror/cassandra/3.11.3/apache-cassandra-3.11.3-bin.tar.gz
-tar -xvf apache-cassandra-3.11.3-bin.tar.gz cassandra
-
+tar -xvf apache-cassandra-3.11.3-bin.tar.gz 
+sudo chmod 777 apache-cassandra-3.11.3/*
+cd apache-cassandra-3.11.3/bin 
+./cassandra -f
