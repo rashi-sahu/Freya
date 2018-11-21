bacc3d1ab2ded98c9c0e215169d5c9b642bbd52c
Abhishek Sah <abhi.sah.97@gmail.com>
2018-11-21 18:56:07 +0530

    [Abhishek] Adds route to fetch data from cassandra

--- database_gateway/app.js
+++ database_gateway/app.js
@@ -1,7 +1,23 @@
 const express = require('express')
 const app = express()
 const port = 3000
+var assert = require('assert');
+const cassandra = require('cassandra-driver');
+var contactPoints = ['127.0.0.1:9042'];
+var client = new cassandra.Client({ contactPoints:contactPoints, keyspace: 'lambda_arch' });
+
+client.connect(function (err) {
+  assert.ifError(err);
+});
 
 app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get('/gateway', async function (req, res) {
+    const query = "select * from emp";
+    client.execute(query, function(err, result) {
+        assert.ifError(err);
+        res.send(JSON.stringify(result));
+    });
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
