5e5f419f8ac35bcf567b55a1041fef55bcd84473
rashi-sahu <rashi.sahu13@gmail.com>
2018-11-22 14:56:46 +0530

    [Rashi] Adds POST route & controller to insert data into db

--- database_gateway/app.js
+++ database_gateway/app.js
@@ -5,10 +5,15 @@ var assert = require('assert');
 const cassandra = require('cassandra-driver');
 var contactPoints = ['127.0.0.1:9042'];
 var client = new cassandra.Client({ contactPoints:contactPoints, keyspace: 'lambda_arch' });
-
+var methodOverride = require('method-override');
+var bodyParser = require('body-parser');
 client.connect(function (err) {
   assert.ifError(err);
 });
+
+app.use(methodOverride());
+app.use(bodyParser.json());    
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
@@ -17,6 +22,18 @@ app.get('/gateway', async function (req, res) {
     client.execute(query, function(err, result) {
         assert.ifError(err);
         res.send(JSON.stringify(result));
+    });
+});
+
+app.post('/gateway', function (req, res) {
+    var postData = req.body;
+    var tableName = postData.table;
+    var fieldNames = postData.fields;
+    var rowData = postData.row;    
+    const query = `INSERT INTO ${tableName} ( ${fieldNames} ) VALUES ( ${rowData} ) `;
+    client.execute(query, function(err, result) {
+        assert.ifError(err);
+        res.send(JSON.stringify("Success"));
     });
 });
 
